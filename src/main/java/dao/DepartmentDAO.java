f5c79e31aedf52d74e328c849ca67fa0b5b38995
Lucky1480 <vasyl.matvijv@gmail.com>
2018-04-27 12:36:13 +0300

    Fix bags

--- src/main/java/dao/DepartmentDAO.java
+++ src/main/java/dao/DepartmentDAO.java
@@ -35,31 +35,7 @@ public class DepartmentDAO {
     public void remove(Department department) {
         session.delete(department);
     }
-    public Lector getDepartmentHead(String name){
-        Department department=get(name);
-        Query query = session.createQuery("select l  from Lector l  " +
-                " l.departments department" +
-                " where lector.id = :departmentHeadID").
-                setLong("departmentHeadID", department.getHead().getId());
-        Lector lector = (Lector) query.uniqueResult();
-        return lector;
-    }
 
-    public void setHead ( String name,Lector lector) {
-        Department department = get(name);
-        if(department==null) return;
-        department.setHead(lector);
-        session.update(department);
-    }
-    public void setLectorToDepartment(String name , Lector lector){
-        Department department= get(name);
-        if(department==null) return;
-
-        Set< Lector>set= department.getLectors();
-        set.add(lector);
-        department.setLectors(set);
-        session.update(department);
-    }
     public List<Department> getAll(){
         Criteria criteria = session.createCriteria(Department.class);
         return criteria.list();
